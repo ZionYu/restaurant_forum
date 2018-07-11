52f8db6eb8be42504978f4719372211cabb9b105
Zion <bone2057@gmail.com>
2018-07-11 12:36:25 +0800

    add friend_list action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :friend_list]
     
   def index
    @users = User.all 
@@ -21,6 +21,10 @@ class UsersController < ApplicationController
   def update
     @user.update(user_params)
     redirect_to user_path(@user)
+  end
+
+  def friend_list
+    @my_friends = @user.all_friends  
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
 
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: "friend_id"
-  has_many :passive_friends, through: :inverse_friends, source: :user
+  has_many :passive_friends, through: :inverse_friendships, source: :user
 
   def admin?
     self.role == "admin"
